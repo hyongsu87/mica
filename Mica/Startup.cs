cba50a648da1b7858ca96b3e280485bce985d942
DESKTOP-IR8GEKG\danielk <hyongsu87@gmail.com>
2021-02-27 16:04:58 +1300

    Add project files.

---
+++ Mica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mica.Startup))]
+namespace Mica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
