a2b556576d0b34bef4231a444d69ea45298c7093
DESKTOP-IR8GEKG\danielk <hyongsu87@gmail.com>
2021-03-01 08:15:27 +1300

    Enabled migration Added model classes

---
+++ Mica/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mica.Models
+{
+    public class Transaction : Model
+    {
+        public Account Account { get; set; }
+        public int AccountId { get; set; }
+        public string Reference { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
