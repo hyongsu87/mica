52f549b61a5a665d9e88b4895f87db5371162c4f
DESKTOP-IR8GEKG\danielk <hyongsu87@gmail.com>
2021-03-07 11:23:04 +1300

    Added useful migration command comments

--- Mica/Models/Model.cs
+++ Mica/Models/Model.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//Migration commands
+//Update-database -TargetMigration:0
+//Update-database
+//add-migration InitialModel - force
+
 namespace Mica.Models
 {
     public abstract class Model
