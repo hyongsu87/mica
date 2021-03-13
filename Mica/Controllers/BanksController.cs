e891166fdc8e135be02d03f1d90f3bac734d1e00
DESKTOP-IR8GEKG\danielk <hyongsu87@gmail.com>
2021-03-13 17:04:49 +1300

    Add banks controller

---
+++ Mica/Controllers/BanksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mica.Controllers
+{
+    public class BanksController : Controller
+    {
+
+        // GET: Banks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
